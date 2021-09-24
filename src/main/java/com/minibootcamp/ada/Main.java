dbb41e2e92a22be32f2c5d25675c7a0eff2624ab
rifqi.t.interns <rifqi.t.interns@aux.gojek.com>
2021-09-24 15:41:02 +0700

    Refactoring commit : change BoardTest.java to CellTest.java and implement Board class

--- src/main/java/com/minibootcamp/ada/Main.java
+++ src/main/java/com/minibootcamp/ada/Main.java
@@ -21,5 +21,60 @@ class Cell {
     }
 }
 
+class Board {
+    int boardWidth;
+    int boardHeight;
+
+    Cell[][] cellDistribution;
+
+    public Board(Cell[][] cellDistribution) {
+        this.cellDistribution = cellDistribution;
+        this.boardWidth = cellDistribution.length;
+        this.boardHeight = cellDistribution[0].length;
+
+        for (int y = 0; y < this.boardWidth; y++) {
+            for (int x = 0; x < this.boardHeight; x++) {
+                this.cellDistribution[x][y] = new Cell(false, 0);
+            }
+        }
+    }
+
+    public void drawBoard() {
+        for (int y = 0; y < boardWidth + 2; y++) {
+            StringBuilder line = new StringBuilder();
+            if (y == 0 || y == boardHeight + 1){
+                line.append(" = ".repeat(Math.max(0, boardWidth + 2)));
+            }else {
+                for (int x = 0; x < boardWidth + 2; x++) {
+                    if (x == 0 || x == boardWidth + 1){
+                        line.append(" | ");
+                    }
+                    else {
+                        if (!cellDistribution[x - 1][y - 1].currentState){
+                            line.append(" . ");
+                        }else {
+                            line.append(" # ");
+                        }
+                    }
+                }
+            }
+            System.out.println(line);
+        }
+        System.out.print("\n");
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+        Cell[][] cellDistribution = new Cell[5][5];
+        Board board = new Board(cellDistribution);
+
+        board.cellDistribution[1][2].currentState = true;
+        board.cellDistribution[2][2].currentState = true;
+        board.cellDistribution[3][2].currentState = true;
+
+        board.drawBoard();
+
+
+    }
 }
--- src/test/java/com/minibootcamp/ada/CellTest.java
+++ src/test/java/com/minibootcamp/ada/CellTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class BoardTest {
+class CellTest {
 
     @Test
     public void testGetCellNewStateGiven0NeighborShouldReturnFalse() {
